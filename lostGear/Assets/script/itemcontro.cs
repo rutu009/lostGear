cf2a4332572fd5e7f3cdf49b77db65ca469d4606
ookawa <example@exampie.com>
2019-11-13 19:11:13 +0900

    add any

---
+++ lostGear/Assets/script/itemcontro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemcontro : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
