f7c6aae9d6d9f8c303e0d1d562f035c975518e95
ookawa <example@exampie.com>
2019-12-07 10:54:25 +0900

    system

--- lostGear/Assets/script/nenryou.cs
+++ lostGear/Assets/script/nenryou.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class nenryou : MonoBehaviour
 {
@@ -11,10 +12,17 @@ public class nenryou : MonoBehaviour
     [SerializeField] float decreaseJump = 0.5f;
     [SerializeField] float tagNenryou = 10f;
     float nowHp;
+    [SerializeField] GameObject maxkaihukupanel;
+    [SerializeField] GameObject kaihukuPanel;
+    [SerializeField] bool kaihuku = true;
+    [SerializeField] bool maxkaihuku = true;
+    Rigidbody2D r;
+    [SerializeField] float texttime = 3.0f;
 
     // Use this for initialization
     void Start()
     {
+        r = GetComponent<Rigidbody2D>();
 
         //hpSlider = GetComponent<Slider>(); //古の記憶（いじるな）
 
@@ -40,18 +48,39 @@ public class nenryou : MonoBehaviour
             hpSlider.value -= decreaseJump;
             Debug.Log(hpSlider.value);
         }
+        if (r.bodyType == RigidbodyType2D.Static & Input.GetKeyDown(KeyCode.Space))
+        {
+            r.bodyType = RigidbodyType2D.Dynamic;
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         float tagNenryouMax = hpSlider.maxValue - hpSlider.value;
         if (collision.gameObject.tag == "nenryouMax")
         {
+            if (maxkaihuku)
+            {
+                maxkaihukupanel.SetActive(true);
+                maxkaihuku = false;
+                r.bodyType = RigidbodyType2D.Static;
+                Destroy(maxkaihukupanel,texttime);
+            }
+
             hpSlider.value += tagNenryouMax;
         }
         if (collision.gameObject.tag == "nenryou")
         {
+            if (kaihuku)
+            {
+                kaihukuPanel.SetActive(true);
+                kaihuku = false;
+                r.bodyType = RigidbodyType2D.Static;
+                Destroy(kaihukuPanel, texttime);
+            }
             hpSlider.value += 10;
         }
+
+        
     }
 
     // Update is called once per frame
