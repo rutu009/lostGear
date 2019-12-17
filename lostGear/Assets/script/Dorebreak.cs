5590a419cd1cfe0a3998561a75ecb562f88b7aa8
ookawa <example@exampie.com>
2019-12-17 13:30:22 +0900

    beta

---
+++ lostGear/Assets/script/Dorebreak.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dorebreak : MonoBehaviour {
+    public Animator animator;
+    bool m_flag = false;
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (m_flag)
+        {
+            animator.SetBool("open",true);
+        }
+    }
+    public void Onflag()
+    {
+        m_flag = true;
+    }
+}
---
+++ lostGear/Assets/script/bottonopen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottonopen : MonoBehaviour {
+    GameObject door;
+    Dorebreak script;
+    [SerializeField] float destroyTime = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        door = GameObject.Find("Door");
+        script = door.GetComponent<Dorebreak>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("open");
+        script.Onflag();
+        Destroy(this.gameObject,destroyTime);
+    }
+}
